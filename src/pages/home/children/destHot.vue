4f0b50af084ee9bd7fadcd51fdee0959d9a8fe4a
nkszwhb <1346788819@qq.com>
2019-11-19 21:30:14 +0800

    解决home滚动

--- src/pages/home/children/destHot.vue
+++ src/pages/home/children/destHot.vue
@@ -3,16 +3,17 @@
       <div class="mode-title">
           <h3>热门目的地</h3>
       </div>
-      <!-- <div class="dest-scroll-x"> -->
+      <div class="dest-scroll-x" ref="desthot"> 
           <div class="dest-hot-items">
               <destItem v-for="(item,index) in destList" :key="item.index" :data="item" class="dest-hot-item"/>
           </div>
-      <!-- </div> -->
+      </div>
   </div>
 </template>
 
 <script>
 import destItem from './destHot-item'
+import BScroll from 'better-scroll'
 export default {
     components:{
         destItem
@@ -25,7 +26,19 @@ export default {
     },
     created(){
     //   console.log(this.destList);
-    }
+    },
+	mounted(){
+		this.desthotscroll = new BScroll(this.$refs.desthot,{
+			tap: true,
+			click: true,
+			scrollX: true,
+			scrollY: false
+		});
+		this.desthotscroll.refresh();
+		this.desthotscroll.on('beforeScrollStart', ()=>{
+		  this.desthotscroll.refresh();
+		});
+	}
 }
 </script>
 
@@ -48,17 +61,20 @@ export default {
             margin: 0;
         }
     }
-    // .dest-scroll-x{
-    //     width: 100%;
-    //     overflow: hidden;
-    //     height: auto;
+    .dest-scroll-x{
+        width: 100%;
+        overflow: hidden;
+		box-sizing: border-box;
+        // height: auto;
+	}
     .dest-hot-items{
-        width:100%;
+        // width:100%;
         height:140px;
         white-space: nowrap;
-        overflow-x: auto;
-        overflow-y: hidden;
-        padding-left: 24px;
+		width: 900px;
+        // overflow-x: auto;
+        // overflow-y: hidden;
+        padding: 0 24px;
         // .dest-hot-item{
         //     float:left;
         // }
