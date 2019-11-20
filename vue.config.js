c0877b96481eff267e8b609766048ab2c59b0676
nkszwhb <1346788819@qq.com>
2019-11-20 19:40:30 +0800

    4565465

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,6 @@ module.exports = {
       '/api/user': {
         target: 'http://10.20.152.32:5000'
       },
-	  '/api/order':{
-		  target: 'http://10.20.152.32:5000'
-	  },
       '/api': {
         target: 'https://app.toursforfun.com'
       }
